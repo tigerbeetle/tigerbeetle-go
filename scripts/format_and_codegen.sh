d41ec1c99eae91ae6d1f96327ca9b41060337217
Phil Eaton <phil@eatonphil.com>
2022-10-26 16:18:02 +0000

    Fix shellcheck

--- scripts/format_and_codegen.sh
+++ scripts/format_and_codegen.sh
@@ -4,6 +4,6 @@ set -eu
 
 GO_IMAGE=golang:1.19
 
-docker run -v $(pwd):/wrk "$GO_IMAGE" bash -c "cd /wrk && gofmt -w -s ."
+docker run -v "$(pwd):/wrk" "$GO_IMAGE" bash -c "cd /wrk && gofmt -w -s ."
 
-docker run -v $(pwd):/wrk "$GO_IMAGE" bash -c "go install golang.org/x/tools/cmd/stringer@latest && go generate /wrk/pkg/types/main.go"
+docker run -v "$(pwd):/wrk" "$GO_IMAGE" bash -c "go install golang.org/x/tools/cmd/stringer@latest && go generate /wrk/pkg/types/main.go"
